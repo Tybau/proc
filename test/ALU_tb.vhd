8640f3635edc824778654c491c29004675463106
Tybau <gomusic.sisium@gmail.com>
2018-10-05 16:09:09 +0200

    ALU_tb

--- test/ALU_tb.vhd
+++ test/ALU_tb.vhd
@@ -9,19 +9,37 @@ architecture behavior of ALU_tb is
 	signal OP_i : std_logic_vector(1 downto 0) := "00";
 	signal A_i, B_i : std_logic_vector(31 downto 0);
 	signal S_i : std_logic_vector(31 downto 0);
-	signal N_i : std_logic := '0';
+	signal N_i : std_logic;
 	begin
 		ALU_I : entity work.ALU port map(
 			OP => OP_i, A => A_i, B => B_i, S => S_i, N => N_i
 		);
 		process
 		begin
-			report "Test 1 :";
 			OP_i <= "00";
 			A_i <= x"00000001";
 			B_i <= x"00000002";
 			wait for 10 ns;
-			assert S_i = x"00000003" report "Erreur 1" severity error;
+
+			OP_i <= "01";
+			A_i <= x"00000005";
+			B_i <= x"00000003";
+			wait for 10 ns;
+
+			OP_i <= "10";
+			A_i <= x"00000005";
+			B_i <= x"00000003";
+			wait for 10 ns;
+
+			OP_i <= "10";
+			A_i <= x"00000003";
+			B_i <= x"00000005";
+			wait for 10 ns;
+
+			OP_i <= "11";
+			A_i <= x"00000003";
+			B_i <= x"00000005";
+			wait for 10 ns;
 
 			wait;
 		end process;
