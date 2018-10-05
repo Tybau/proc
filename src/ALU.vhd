65f31eb0a62092cdef27ea2542f00722251f41ef
Tybau <gomusic.sisium@gmail.com>
2018-10-05 15:32:43 +0200

    Continue ALU

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -5,29 +5,25 @@ use ieee.numeric_std.all;
 entity ALU is
 	port (
 		OP : in std_logic_vector(1 downto 0);
-		A, B : in integer;
-		S : out integer;
+		A, B : in std_logic_vector(31 downto 0);
+		S : out std_logic_vector(31 downto 0);
 		N : out std_logic
 	);
 end entity ALU;
 
 architecture behavior of ALU is
-	signal R : integer;
+	signal R : std_logic_vector(31 downto 0);
 	begin
 		process(OP, A, B)
 		begin
 			case OP is
-				when "00" => R <= A + B; 	-- ADD
+				when "00" => R <= std_logic_vector(signed(A) + signed(B)); 	-- ADD
 				when "01" => R <= B; 		-- B
-				when "10" => R <= A - B; 	-- SUB
+				when "10" => R <= std_logic_vector(signed(A) - signed(B)); 	-- SUB
 				when "11" => R <= A; 		-- A
 				when others => report "unreachable" severity failure;
 			end case;
-			if R > 0 then
-				N <= '1';
-			else
-				N <= '0';
-			end if;
+			N <= R(31);
 			S <= R;
 		end process;
 end architecture behavior;
--- test/ALU_tb.vhd
+++ test/ALU_tb.vhd
@@ -1,14 +1,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity ALU_tb is
 end entity ALU_tb;
 
 architecture behavior of ALU_tb is
-	signal OP_i : std_logic_vector(1 downto 0);
-	signal A_i, B_i : integer;
-	signal S_i : integer;
-	signal N_i : std_logic;
+	signal OP_i : std_logic_vector(1 downto 0) := "00";
+	signal A_i, B_i : std_logic_vector(31 downto 0) := x"00000000";
+	signal S_i : std_logic_vector(31 downto 0) := x"00000000";
+	signal N_i : std_logic := '0';
 	begin
 		ALU_I : entity work.ALU port map(
 			OP => OP_i, A => A_i, B => B_i, S => S_i, N => N_i
@@ -17,10 +18,10 @@ architecture behavior of ALU_tb is
 		begin
 			report "Test 1 :";
 			OP_i <= "00";
-			A_i <= 2;
-			B_i <= 1;
+			A_i <= x"00000001";
+			B_i <= x"00000002";
 			wait for 10 ns;
-			assert S_i = 3 report "Erreur 1" severity error;
+			assert S_i = x"00000003" report "Erreur 1" severity error;
 
 			wait;
 		end process;
