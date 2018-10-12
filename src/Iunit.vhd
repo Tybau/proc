c696f11ce1ba9fea15739275497626020ebf322c
Tybau <gomusic.sisium@gmail.com>
2018-10-12 14:11:14 +0200

    Fix clk

--- src/Iunit.vhd
+++ src/Iunit.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity Iunit is
 	port(
+		clk: in std_logic;
 		offset: in std_logic_vector(23 downto 0);
 		nPCsel: in std_logic;
 		instruction: out std_logic_vector(31 downto 0)
